059a8da8d68243ae32670ee14d89ffa4d2f09762
Dmitrii Dimandt <dmitrii@dmitriid.com>
2019-07-11 12:41:59 +0200

    Future config

--- config/config.exs
+++ config/config.exs
@@ -8,19 +8,40 @@
 use Mix.Config
 
 # Configures the endpoint
-config :phreak, PhreakWeb.Endpoint,
-  url: [host: "localhost"],
-  secret_key_base: "xEGlzNEpRkNrKZiPnYzIP7UuELrn6he+G716tKFoRs8qM11MtgVYkxynmpfn6ozP",
-  render_errors: [view: PhreakWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Phreak.PubSub, adapter: Phoenix.PubSub.PG2]
+config :phreak,
+       PhreakWeb.Endpoint,
+       url: [
+         host: "localhost"
+       ],
+       secret_key_base: "xEGlzNEpRkNrKZiPnYzIP7UuELrn6he+G716tKFoRs8qM11MtgVYkxynmpfn6ozP",
+       render_errors: [
+         view: PhreakWeb.ErrorView,
+         accepts: ~w(html json)
+       ],
+       pubsub: [
+         name: Phreak.PubSub,
+         adapter: Phoenix.PubSub.PG2
+       ],
+       live_view: [
+         signing_salt: "SECRET_SALT"
+       ],
+       pubsub: [
+         name: Phreak.PubSub,
+         adapter: Phoenix.PubSub.PG2
+       ]
 
 # Configures Elixir's Logger
-config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
+config :logger,
+       :console,
+       format: "$time $metadata[$level] $message\n",
+       metadata: [:request_id]
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
+
+config :phreak,
+       :kube,
+       config: "/Users/dmitrii.dimandt/.kube/config"
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
